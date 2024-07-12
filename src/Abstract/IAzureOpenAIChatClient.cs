97b4f99d27e408490926cd9359908d21471753b9
Jake Soenneker <jake@soenneker.com>
2024-07-12 05:47:38 -0500

    Initial commit

---
+++ src/Abstract/IAzureOpenAIChatClient.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.AI.OpenAI;
+using OpenAI.Chat;
+
+namespace Soenneker.Azure.OpenAI.Client.Chat.Abstract;
+
+/// <summary>
+/// An async thread-safe singleton for the Azure OpenAI Chat (completions) client
+/// </summary>
+// ReSharper disable once InconsistentNaming
+public interface IAzureOpenAIChatClient : IDisposable, IAsyncDisposable
+{
+    /// <summary>
+    /// Not required, but can be used to set the model and options for the client
+    /// </summary>
+    /// <param name="model"></param>
+    /// <param name="options"></param>
+    void SetOptions(string model, AzureOpenAIClientOptions options);
+
+    ValueTask<ChatClient> Get(CancellationToken cancellationToken = default);
+}
---
+++ test/AzureOpenAIChatClientTests.cs
@@ -0,0 +1,18 @@
+using Soenneker.Azure.OpenAI.Client.Chat.Abstract;
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Soenneker.Azure.OpenAI.Client.Chat.Tests;
+
+[Collection("Collection")]
+// ReSharper disable once InconsistentNaming
+public class AzureOpenAIChatClientTests : FixturedUnitTest
+{
+    private readonly IAzureOpenAIChatClient _util;
+
+    public AzureOpenAIChatClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+        _util = Resolve<IAzureOpenAIChatClient>(true);
+    }
+}
